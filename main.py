a98393f2f59d88802d28d9866996f8e0dfe01c78
Thiago <Thiagogferronato@gmail.com>
2021-12-10 09:10:01 -0300

    Ta em milhoes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from datetime import datetime
 import matplotlib.ticker as tick
+from matplotlib.ticker import MaxNLocator
 
 
 data = pd.read_csv('covid_19_data.csv')
@@ -13,38 +14,28 @@ yval = []
 xvala = []
 yvala = []
 
+
 def plotar(filtro,filtrado,vy):
-    for i in range(len(data['SNo'])):
+    for i in range(datetime.strptime((data['ObservationDate'][i]))):
         if str(data[filtro][i]) == filtrado:
-            yval.append(float(data[vy][i]))
+            yval.append(int(data[vy][i]))
             auxdata = datetime.strptime((data['ObservationDate'][i]), '%m/%d/%Y')
             xval.append(auxdata)
 
     plt.plot(xval, yval, label=filtrado)
+    print(yval)
     xval.clear()
     yval.clear()
 
 # plt.ticklabel_format(axis="y",style='sci',scilimits=(0,0))
 plotar('Province/State','Santa Catarina','Confirmed')
-plotar('Province/State','Bahia','Confirmed')
+# plotar('Province/State','Bahia','Confirmed')
 plotar('Province/State','Rio de Janeiro','Confirmed')
-plotar('Province/State','Rondonia','Confirmed')
+# plotar('Province/State','Rondonia','Confirmed')
 plotar('Province/State','Parana','Confirmed')
-
-# for i in range(len(data['SNo'])):
-#     if str(data['Province/State'][i]) == 'Santa Catarina':
-#         yval.append(float(data['Confirmed'][i]))
-#         auxdata = datetime.strptime((data['ObservationDate'][i]), '%m/%d/%Y')
-#         xval.append(auxdata)
-# for i in range(len(data['SNo'])):
-#     if str(data['Province/State'][i]) == 'Bahia':
-#         yvala.append(float(data['Confirmed'][i]))
-#         auxdata = datetime.strptime((data['ObservationDate'][i]), '%m/%d/%Y')
-#         xvala.append(auxdata)
-# x = range(len(xval))
-# plt.plot(xval, yval, label = "line 1")
-# plt.plot(xvala, yvala, label = "line 2")
-# plt.locator_params(axis='y', nbins=10)
+plotar('Province/State','Sao Paulo','Confirmed')
+# ax = plt.gca()
+# ax.set_ylim([0, 5000000])
 plt.legend()
-# plt.show()
-plt.savefig('1.png')
+plt.show()
+# plt.savefig('1.png')
